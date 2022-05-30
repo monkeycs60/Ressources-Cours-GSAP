ee375d16d79a5d405c83f2f9fcd087059123aec9
monkeycs60 <clement.serizay@gmail.com>
2022-05-30 16:25:41 +0200

    projet 2 terminé

---
+++ 3. Projets/1.ProjetsDeDépart/2. chargement GSAP/app.js
@@ -0,0 +1,40 @@
+const blocLoad = document.querySelector('.load-container');
+const video = document.querySelector('video');
+
+const imgContainer = document.querySelector('.images-container');
+
+const image1 = document.querySelector('.flip-img1');
+const image2 = document.querySelector('.flip-img2');
+
+const titre1 = document.querySelector('.bloc-txt');
+const titre2 = document.querySelector('.bloc-txt h2');
+
+const bgRed = document.querySelector('.flip');
+
+const tl = gsap.timeline({
+    defaults: {
+        duration: 0.5,
+        ease: 'power1'
+    }
+});
+
+tl
+.to(imgContainer, {keyframes : [
+    {height: '100%'},
+    
+    
+]})
+.to (titre1, {keyframes : [
+    {translateY: '10%'},
+    {height: '20%'},
+]})
+.to (titre2, {keyframes : [
+    {translateY: '10%'},
+    
+]})
+.to (bgRed,  {translateY: '-100%', duration : 3})
+.to (image1, {autoAlpha: 0}, '-=2.2')
+.to (blocLoad, {autoAlpha: 0})
+//play the video after that
+.to (video, {autoAlpha: 1, onComplete: () => video.play()}, '-=0.5')
+
